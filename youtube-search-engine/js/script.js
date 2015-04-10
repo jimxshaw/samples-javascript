240859f85ecd94181b7034c9ba22e85f276e6cc8
Jim Shaw <jimxshaw@gmail.com>
2015-04-09 22:41:53 -0500

    finished search engine javascript

--- youtube-search-engine/js/script.js
+++ youtube-search-engine/js/script.js
@@ -60,8 +60,142 @@ function search() {
         // Display results
         $('#results').append(output);
       });
+
+      var buttons = getButtons(nextPageToken, prevPageToken);
+
+      // Display buttons
+      $('#buttons').append(buttons);
     }
   );
+}
+
+// Next page function
+function nextPage() {
+  var token = $('#next-button').data('token');
+  var q = $('#next-button').data('query');
+
+  // Clear results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  // Get form input
+  q = $('#query').val();
+
+  // Run GET request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search", {
+      part: 'snippet, id',
+      q: q,
+      pageToken: token,
+      type: 'video',
+      key: 'AIzaSyC8ow_45a6Evhl4PNCyQuNthA0zGrYCn_Y'
+    },
+    function(data) {
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      // Log data
+      console.log(data);
+
+      $.each(data.items, function(i, item) {
+        // Get output
+        var output = getOutput(item);
+
+        // Display results
+        $('#results').append(output);
+      });
+
+      var buttons = getButtons(nextPageToken, prevPageToken);
+
+      // Display buttons
+      $('#buttons').append(buttons);
+    }
+  );
+}
+
+// Prev page function
+function prevPage() {
+  var token = $('#prev-button').data('token');
+  var q = $('#prev-button').data('query');
+
+  // Clear results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  // Get form input
+  q = $('#query').val();
+
+  // Run GET request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search", {
+      part: 'snippet, id',
+      q: q,
+      pageToken: token,
+      type: 'video',
+      key: 'AIzaSyC8ow_45a6Evhl4PNCyQuNthA0zGrYCn_Y'
+    },
+    function(data) {
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      // Log data
+      console.log(data);
+
+      $.each(data.items, function(i, item) {
+        // Get output
+        var output = getOutput(item);
+
+        // Display results
+        $('#results').append(output);
+      });
+
+      var buttons = getButtons(nextPageToken, prevPageToken);
+
+      // Display buttons
+      $('#buttons').append(buttons);
+    }
+  );
+}
+
+// Build output
+function getOutput(item) {
+  var videoId = item.id.videoId;
+  var title = item.snippet.title;
+  var description = item.snippet.description;
+  var thumb = item.snippet.thumbnails.high.url;
+  var channelTitle = item.snippet.channelTitle;
+  var videoDate = item.snippet.publishedAt;
+
+  // Build output string
+  var output = '<li>' +
+    '<div class="list-left">' +
+    '<img src="' + thumb + '">' +
+    '</div>' +
+    '<div class="list-right">' +
+    '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/' + videoId + '">' + title + '</a></h3>' +
+    '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
+    '<p>' + description + '</p>' + 
+    '</div>' +
+    '</li>' +
+    '<div class="clearfix"></div>' +
+    '';
+
+    return output;
+}
+
+// Build the buttons
+function getButtons(nextPageToken, prevPageToken) {
+  if (!prevPageToken) {
+    var btnoutput = '<div class="button-container">' + 
+      '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '" onclick="nextPage();">Next Page</button></div>';
+  }
+  else {
+    var btnoutput = '<div class="button-container">' +
+      '<button id="prev-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '" onclick="prevPage();">Prev Page</button>' +
+      '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '" onclick="nextPage();">Next Page</button></div>';
+  }
+
+  return btnoutput;
 }
 
 
