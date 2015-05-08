6a61e0df2ac16de7f8654cad5f5473c27657098e
Jim Shaw <jimxshaw@gmail.com>
2015-05-08 17:59:58 -0500

    added lightbox & responsiveness

--- portfolio-gallery/js/script.js
+++ portfolio-gallery/js/script.js
@@ -27,6 +27,46 @@ $(document).ready(function() {
     // Stop link behavior
     return false;
   });
+
+  // Mouse enter overlay
+  $('ul#gallery li').on('mouseenter', function() {
+    // Get data attribute values
+    var title = $(this).children().data('title');
+    var desc = $(this).children().data('desc');
+
+    // Validation
+    if (desc == null) {
+      desc = 'Click to Enlarge';
+    }
+
+    if (title == null) {
+      title = '';
+    }
+
+    // Create overlay div
+    $(this).append('<div class="overlay"></div>');
+
+    // Get the overlay div
+    var overlay = $(this).children('.overlay');
+
+    // Add html to overlay
+    overlay.html('<h3>' + title + '</h3><p>' + desc + '</p>');
+
+    // Fade in the overlay
+    overlay.fadeIn(250);
+  });
+
+  // Mouse leave overlay
+  $('ul#gallery li').on('mouseleave', function() {
+    // Create overlay div
+    $(this).append('<div class="overlay"></div>');
+
+    // Get the overlay div
+    var overlay = $(this).children('.overlay');
+
+    // Fade out overlay
+    overlay.fadeOut(250);
+  });
 });
 
 
